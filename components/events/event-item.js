462dbc71ba70700f9f3276e2fdfe263f98fd06f3
gaganweb <gaganwebsites@gmail.com>
2023-02-13 16:16:28 +0400

    Commit

---
+++ components/events/event-item.js
@@ -0,0 +1,41 @@
+import classes from './event-item.module.css';
+import DateIcon from '../icons/date-icon.js';
+import AddressIcon from '../icons/address-icon.js';
+import ArrowRightIcon from '../icons/arrow-right-icon.js';
+import Button from '../ui/button';
+
+function EventItem(props) {
+    const { title, image, date, location, id } = props;
+
+    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    });
+    const formattedAddress = location.replace(', ', '\n');
+    const exploreLink = `/events/${id}`;
+    return ( 
+    <li className={classes.item}>
+        <img src={'/' + image} alt={title} />
+        <div className={classes.content}>
+            <div className={classes.summary}><h2>{title}</h2></div>
+            <div className={classes.date}>
+                <DateIcon />
+                <time>{humanReadableDate}</time>
+            </div>
+            <div className={classes.address}>
+                <AddressIcon />
+                <address>{formattedAddress}</address>
+            </div>
+        </div>
+        <div className={classes.actions}>
+            <Button link={exploreLink}>
+                <span>Explore Event</span>
+                <span className={classes.icon}><ArrowRightIcon /></span>
+                </Button>
+        </div>
+    </li>
+    );
+}
+
+export default EventItem;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
+
+function HomePage() {
+    const FeaturedEvents = getFeaturedEvents();
+    return (
+        <div>
+            <EventList items={FeaturedEvents} />
+        </div>
+    );
+}
+
+export default HomePage;
